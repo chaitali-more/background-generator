51b2180742e5735b48fbebc3e13fcf41c9cd5efc
chaitali more <chaitali1266@gmail.com>
2020-03-26 18:33:43 +0530

    Background Generator project Files

---
+++ backgroundGenerator.js
@@ -0,0 +1,15 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient") 
+// console.log(body);
+// body.style.background="red";
+function bg(){
+	// console.log(color1.value);
+	body.style.background="linear-gradient(to right ," + color1.value + "," + color2.value +")";
+
+	css.textContent= body.style.background + ";" ;
+} 
+color1.addEventListener("input",bg);
+color2.addEventListener("input",bg);
+
